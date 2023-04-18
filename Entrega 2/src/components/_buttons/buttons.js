ed5945a2c5e8c9226d27cad82395ccbd27efdfd7
fariaspatriciodaniel <108495763+fariaspatriciodaniel@users.noreply.github.com>
2023-04-18 00:22:24 -0300

    Delete Entrega 2 directory

--- Entrega 2/src/components/_buttons/buttons.js
+++
@@ -1,8 +0,0 @@
-import styles from './buttons.module.css';
-
-const ButtonNav = ({text}) => {
-    return <button className={`${styles.bgButton} ${styles.buttonReset} ${styles.buttonSize} ${styles.borderButton} ${styles.fontBlack}`}>{text}</button>
-};
-
-export default ButtonNav;
-
